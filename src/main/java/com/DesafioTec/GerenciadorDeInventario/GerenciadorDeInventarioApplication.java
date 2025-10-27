ea691d8874ad5e0509b3889faffc4b49e4add33d
Arthur Simões <amsbsimoes@gmail.com>
2025-10-27 13:26:49 -0300

    first commit

---
+++ src/main/java/com/DesafioTec/GerenciadorDeInventario/GerenciadorDeInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.DesafioTec.GerenciadorDeInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DesafioTec/GerenciadorDeInventario/GerenciadorDeInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DesafioTec.GerenciadorDeInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
